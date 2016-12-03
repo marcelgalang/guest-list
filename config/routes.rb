e49647c65fcdedfe36e2356c618ead2e44747076
marcelgalang <marcelgalang@gmail.com>
2016-12-02 21:55:59 -0500

    Changes for present and absent toggle

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
-  # get 'guests/create'
-  #
-  # resources :lists do
-  #   resources :guests
-  # end
-  #
-  resources :users
-  #
-  # # roots
-  # root "static_pages#home"
-  #
-  # # sessions
-  # get "/signin", to: "sessions#new"
-  # post "/sessions/create", to: "sessions#create"
-  # delete "/signout", to: "sessions#destroy"
 
+  resources :users
 
   get '/auth/facebook/callback' => 'sessions#create'
 
