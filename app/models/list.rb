cd666793945bcee81188fc225622abc295b088c3
marcelgalang <marcelgalang@gmail.com>
2017-01-10 13:05:38 -0500

    List class methods added in List model

--- app/models/list.rb
+++ app/models/list.rb
@@ -14,26 +14,51 @@ class List < ApplicationRecord
 
   validates :name, :presence => true, :length => {:minimum => 3}
 
-    def guests_attributes=(guests_hashes)
-      guests_hashes.each do |i, guest_attributes|
-       if guest_attributes[:name].present?
-         guest= Guest.find_or_create_by(name: guest_attributes[:name])
-         # if !self.guests.include?(guest)
-         #   self.post_categories.build(:category => category)
-         # end
-       end
-     end
-   end
+  #   def guests_attributes=(guests_hashes)
+  #     guests_hashes.each do |i, guest_attributes|
+  #      if guest_attributes[:name].present?
+  #        guest= Guest.find_or_create_by(name: guest_attributes[:name])
+  #        # if !self.guests.include?(guest)
+  #        #   self.post_categories.build(:category => category)
+  #        # end
+  #        end
+  #      end
+  #   end
 
-    def self.permissable(*args)
-    args.each do |action|
-      # action #=> :view
-      define_method "#{action}able?" do
-        self.permission <= PERMISSIONS[action]
-      end
-    end
+  #   def self.permissable(*args)
+  #   args.each do |action|
+  #     # action #=> :view
+  #     define_method "#{action}able?" do
+  #       self.permission <= PERMISSIONS[action]
+  #     end
+  #   end
+  # end
+
+  # permissable :view, :edit, :destroy
+
+  def head_count(list)
+    list.guests.size
   end
 
-  permissable :view, :edit, :destroy
+  def percent_attending(list)
+    attending =[]
+    list.guests.each do |guest|
+     if guest.status = 1
+     attending << guest
+    end
+    list.head_count/attending
+  end
+end
+
+def most_invited
+  @lists.max_by {|list| head_count(list)}
+end
+
+def best_attendance
+  @lists.max_by do |list|
+    head_count(list)/percent_attending(list)
+  end
+end
+
 
 end
