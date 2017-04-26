064411d16fb08c24d0b759e6357f2885eb9a4a20
github username <github email address>
2017-04-26 16:58:09 -0400

    Save Gemfile changes

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,16 +3,16 @@ class Guest < ApplicationRecord
   belongs_to :list
 
   validates :name, :presence => true, :length => {:minimum => 1}
-  STATUS = {
-    :unattending => 0,
-    :attending => 1
-  }
+  # STATUS = {
+  #   :unattending => 0,
+  #   :attending => 1
+  # }
 
-  def attending?
-     self.status == STATUS[:attending]
-   end
-
-   def unattending?
-     self.status == STATUS[:unattending]
-   end
+  # def attending?
+  #    self.status == STATUS[:attending]
+  #  end
+  #
+  #  def unattending?
+  #    self.status == STATUS[:unattending]
+  #  end
 end
