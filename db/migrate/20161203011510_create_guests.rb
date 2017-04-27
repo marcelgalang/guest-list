99324d68199d471f0f08c91f7a71a9d257230705
github username <github email address>
2017-04-26 20:27:27 -0400

    Change migrations for guests

--- db/migrate/20161203011510_create_guests.rb
+++ db/migrate/20161203011510_create_guests.rb
@@ -2,7 +2,7 @@ class CreateGuests < ActiveRecord::Migration[5.0]
   def change
     create_table :guests do |t|
       t.string :name
-      t.boolean :status, default: 0
+      t.boolean :status
       t.integer  :list_id
       t.timestamps
     end
--- db/migrate/20161205142612_add_status_to_guests.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToGuests < ActiveRecord::Migration[5.0]
-  def change
-    change_column :guests, :status, :integer, :default => 0
-  end
-end
